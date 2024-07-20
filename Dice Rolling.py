1c3f4a8a07632b2f135a0fe5cf0d8ed84cf7c0e6
Sourav-Coding <souravmukherjee1584@gmail.com>
2024-07-20 20:48:14 +0530

    Adding the new project

---
+++ Dice Rolling.py
@@ -0,0 +1,9 @@
+import random
+n=int(input("Enter number of dice you want to throw: "))
+for i in range(n):
+    rand=random.randint(1,6)
+    spin=random.randint(1,5)
+    for i in range(spin):
+        print("Dice is Rolling .....")
+    print("Your Number is :",rand)
+
